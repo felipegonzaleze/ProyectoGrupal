a74f1329d74b0c6876b35036a05ba8564340ecf6
felipegonzaleze <fgonech@gmail.com>
2025-06-05 00:53:56 -0400

    feat: se cambia lógica y estilos de Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,9 @@
-import imagenForge from "../assets/Fundacion-Forge-Logo.svg";
 import { Megamenu } from "./Megamenu.jsx";
 import "./Header.css";
 
 export const Header = () => {
   return (
     <div className="container">
-      <img
-        className="logo-forge"
-        alt="Logo de la fundación forge"
-        src={imagenForge}
-      />
       <Megamenu className="menu" />
     </div>
   );
