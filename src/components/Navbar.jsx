baf61f2802ac179b44963ea40ad392fd20203263
felipegonzaleze <fgonech@gmail.com>
2025-06-05 00:54:40 -0400

    feat: se cambia estructura y estilos de Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import forgeLogo from '../assets/Fundacion-Forge-Logo-sinfrase.svg';
+import forgeLogo from '../assets/Fundacion-Forge-Logo.svg';
 import './Navbar.css';
 
 export const Navbar = () => {
     return (
         <div className="nav-container">
             <img id="forgeLogo" src={forgeLogo} alt="Logo Fundación Forge" />
-            <h1>Bienvenido a la adultez</h1>
             <div className="botones">
-                <a href="#Registrarse">Registrarse</a>
-                <a href="#Login">Login</a>
+                <a href="">About us</a>
+                <a href="">Contáctanos</a>
+                <a href="">Mapa</a>
             </div>
         </div>
     );
