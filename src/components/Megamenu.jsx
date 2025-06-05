c361619603ca1341f4b4e63c28d9e79c25ef83aa
felipegonzaleze <fgonech@gmail.com>
2025-06-05 00:55:28 -0400

    feat: se separa lógica de constantes en Megamenu para mejor orden

--- src/components/Megamenu.jsx
+++ src/components/Megamenu.jsx
@@ -1,68 +1,14 @@
 import { Menu } from "antd";
+import './Megamenu.css';
+import { items } from "../constants/constants.js";
 
 export const Megamenu = () => {
   return (
     <>
       <Menu
+      className="menu"
         mode="horizontal"
-        items={[
-          {
-            label: "Educación",
-            key: "Educación",
-            children: [
-              {
-                label: "Educación Financiera",
-                key: "Educacion-Financiera",
-              },
-              {
-                label: "Educación Continua",
-                key: "Educacion-Continua",
-              },
-            ],
-          },
-          {
-            label: "Mundo Laboral",
-            key: "Mundo-Laboral",
-            children: [
-              {
-                label: "Tips para tus entrevistas",
-                key: "Tips-entrevistas",
-              },
-              {
-                label: "Como armar tu cv",
-                key: "Como-Armar-CV",
-              },
-            ],
-          },
-          {
-            label: "Trámites",
-            key: "Tramites",
-            children: [
-              {
-                label: "Ir al banco",
-                key: "banco",
-              },
-            ],
-          },
-          {
-            label: "Tips para la adultez",
-            key: "Tips-adultez",
-            children: [
-              {
-                label: "Salud mental",
-                key: "salud-mental",
-              },
-              {
-                label: "Haz ejercicio",
-                key: "ejercicio",
-              },
-            ],
-          },
-          {
-            label: "Como ir a un hospital",
-            key: "Como_ir_hospital",
-          },
-        ]}
+        items={items}
       ></Menu>
     </>
   );
---
+++ src/constants/constants.js
@@ -0,0 +1,40 @@
+export const items = [
+  {
+    label: "Educación",
+    key: "educacion",
+    children: [
+      {
+        label: "Educación Financiera",
+        key: "educacion-financiera",
+      },
+      {
+        label: "Educación Continua",
+        key: "educacion-continua",
+      },
+    ],
+  },
+  {
+    label: "Inserción Laboral",
+    key: "insercion-laboral",
+    children: [
+      {
+        label: "Tips para tus entrevistas",
+        key: "tips-entrevistas",
+      },
+      {
+        label: "Como armar tu cv",
+        key: "como-armar-cv",
+      },
+    ],
+  },
+  {
+    label: "Autoconocimiento",
+    key: "autoconocimiento",
+    children: [
+      {
+        label: "Desarrollo de habilidades",
+        key: "desarrollo-habilidades",
+      },
+    ],
+  },
+];
